2d3a9cf6b97b467fa548521816426c84a07c764d
unknown <luna.skywalker@hotmail.com>
2020-01-18 09:44:16 +0100

    THIRDADD

--- GuessNumberGame.py
+++ GuessNumberGame.py
@@ -1,21 +1,18 @@
 import random
 
-nbr=random.randint(1, 100)
-guess= False
+nbr = random.randint(1, 100)
+guess = False
 
-while not guess :
+while not guess:
     try:
         n = int((input("Entrez un nombre entier: ")))
     except Exception :
         print("ERREUR, VEUILLEZ ENTRER UN NOMBRE.")
         continue
-    if n==nbr :
+    if n == nbr:
         print("BRAVO! C'EST BIEN LE CHIFFRE A DEVINER.")
-        guess= True
-    elif n>nbr :
+        guess = True
+    elif n > nbr:
         print("Votre chiffre est supérieur au mien, Réessayez!")
     else:
         print("Votre chiffre est inférieur au mien, Réessayez!")
-
-
-
